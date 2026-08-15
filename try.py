05e505d7ce875a07eed5b5b410527ae57f0946b1
Faust-official <dbk21874140111@outlook.com>
2026-08-16 01:32:33 +0800

    quickly change

---
+++ try.py
@@ -0,0 +1,12 @@
+# 强制UTF-8输出编码
+import sys
+import io
+if sys.stdout.encoding != 'utf-8':
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+# ===========================================================
+# print(f"长度是 {len('hello')}")
+m=0
+for i in range(51):
+    m+=i+0
+print(m)
